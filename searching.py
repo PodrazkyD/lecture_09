187b439a0e6a7ec4741d8561042cdd03e3d29896
PodrazkyD <david.podrazky@seznam.cz>
2022-04-11 12:22:58 +0200

    message

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -13,8 +14,33 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
+        return None
+
+    with open(file_path, "r") as soubor:
+        slovnik = json.load(soubor)
+
+    return slovnik[field]
+
+def linear_search(sequention, number):
+    new_dictionary = {"positions": [],
+                      "count": 0}
+    index = 0
+    while True:
+        if len(sequention) - 1 == index:
+            break
+        elif sequention[index] == number:
+            new_dictionary["positions"].append(index)
+            index = index + 1
+        else:
+            continue
+    new_dictionary["count"] = sequention.count(number)
+
+    return new_dictionary
+
 
 def main():
+    print(linear_search(read_data("sequential.json", "ordered_numbers"), 14))
     pass
 
 
